32db05e525ece21c84239d88b8cba60c8651b19f
selene <selenemu4ozboza@gmail.com>
2026-03-18 16:59:12 +0100

    Paquete del sistema de la ciudad

---
+++ src/SISDECIUDAD/Ciudad.java
@@ -0,0 +1,51 @@
+package SISDECIUDAD;
+import java.util.ArrayList;
+
+public class Ciudad {
+
+	private int dinero;
+	private int energia;
+	private int poblacion;
+	private ArrayList<Edificio>edificios;
+	
+	public Ciudad() {
+		dinero = 5000;
+		energia = 0;
+		poblacion = 1000;
+		edificios = new ArrayList<>();
+	}
+	
+	public void agregarEdificio(Edificio edificio) {
+		edificios.add(edificio);
+	}
+	
+	public ArrayList<Edificio>getEdificios(){
+		return edificios;
+	}
+	
+	public void mostrarEstado (int mes) {
+		System.out.println("\n--- ESTADO DE ECOCITY - MES " + mes + " ---");
+		System.out.println("Recursos: $" + dinero + " | Energía: +" + energia + "MW | Población: " + poblacion);
+		
+		System.out.println("Edificios construidos: ");
+		
+		for(int i = 0; i < edificios.size(); i++) {
+			System.out.println((i + 1) + "." + edificios.get(i));
+		}
+	}
+	
+	public void pasarMes() {
+		
+		energia = 0;
+		for (Edificio edificio : edificios) {
+			
+			edificio.aplicarEfectoMensual();
+			
+			if(edificio instanceof GeneradorRecursos) {
+				GeneradorRecursos generador = (GeneradorRecursos) edificio;
+				energia += generador.producirRecurso();
+			}
+		}
+	}
+	
+}
